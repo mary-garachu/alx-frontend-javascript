30039a1d3455bc06ff71157095b7a91fda2106da
Sonie <garachu.muthoni@gmail.com>
2024-05-01 00:28:18 +0300

    fix errorrs

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-array-constructor */
-export default function getListStudents() {
-	return [
-		{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-		{ id: 2, firstName: 'James', location: 'Columbia' },
-		{ id: 5, firstName: 'Serena', location: 'San Francisco' }
-	]
 
+export default function getListStudents() {
+  const studentOne = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const studentTwo = { id: 2, firstName: 'James', location: 'Columbia' };
+  const studentThree = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+
+  return new Array(studentOne, studentTwo, studentThree);
 }
